f7de51290d4cfbad95cdc66bcc9fc59e51e5801a
Tejas <tejasvispute25@gmail.com>
2023-04-04 15:54:29 +0530

    dark mode feature added

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const hamburg = document.getElementById('hamburger');
 const menu = document.getElementById('menu');
+const body = document.querySelector('body');
+const moon = document.querySelector('#moon');
 
 hamburg.addEventListener("click", () => {
     menu.classList.toggle("hidden")
     hamburg.classList.toggle("text-white")
+
+})
+
+// dark light toggle
+moon.addEventListener("click", () => {
+    body.classList.toggle("dark")
+   
+
 })
